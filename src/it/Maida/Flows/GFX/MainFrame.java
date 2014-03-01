a289bb63577cf19608295e237fdb09afc700d11a
Francesco <Francesco@Francesco-G500S>
2014-03-01 17:29:59 +0100

    prova sul mainFrame, non ho cambiato niente

--- src/it/Maida/Flows/GFX/MainFrame.java
+++ src/it/Maida/Flows/GFX/MainFrame.java
@@ -11,8 +11,7 @@ public class MainFrame extends JFrame {
 
 	private static MainFrame instance;
 	private JPanel contentPanel;
-	private MainPanel mainPanel;
-	
+	private MainPanel mainPanel;	
 	public static MainFrame getInstance() {
 		if ( instance == null )
 			instance = new MainFrame();
